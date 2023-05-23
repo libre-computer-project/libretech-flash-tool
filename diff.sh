af5a420a2fa04af9ef22d89012fd1bca91dabb31
Da Xue <da@lessconfused.com>
2023-05-23 02:28:04 -0400

    diff: add tool

---
+++ diff.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+case "$1" in
+	"dt")
+		target=dts
+		;;
+	"config")
+		target=config
+		;;
+	*)
+		echo "?" >&2
+		exit 1
+		;;
+esac
+
+getURL(){
+	#board
+	#board/v2023.04
+	#board/linux-5.10.y
+	#board/master
+	board="${1%%/*}"
+	if [ "$board" != "$1" ]; then
+		release="${1#*/}"
+		echo "http://boot.libre.computer/vanilla/$board/$release/$board-$release.$target"
+	else
+		echo "http://boot.libre.computer/ci/$1.$target"
+	fi
+}
+
+url1=$(getURL "$2")
+url2=$(getURL "$3")
+echo "$url1"
+echo "$url2"
+diff -y -W $(tput cols) <(curl "$url1") <(curl "$url2") | less
